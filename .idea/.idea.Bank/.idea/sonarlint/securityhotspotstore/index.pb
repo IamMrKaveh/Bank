706fbf23181e2d7c970c3087716a232caaeef1b0
I am MrKaveh <iammrkaveh@gmail.com>
2023-04-30 14:59:29 +0430

    update git ignore

---
+++ .idea/.idea.Bank/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+V
+&Structure/Encryption/EncryptDecrypt.cs,c\0\c01d29dce687d692aa4708d6dbef6d6a65d8d034
+R
+"Structure/Date/Shamsi_TO_Miladi.cs,b\e\bebda96d799ef49d3185edbe6865e7d23698d589
+f
+6Structure/Forms/SelectLanguage/SelectLanguage_Class.cs,b\8\b83855667369e16aa2dee879baa6a76ea7ee52ad
+R
+"Structure/Date/Miladi_TO_Shamsi.cs,0\6\06e01d066ed69366ecfc7ebb9d7fa50711cfd65b
+S
+#Structure/Encryption/MyEncryptor.cs,4\c\4c031282656c7dae85677debebe2e0344a046ef0
---
+++ Structure/Forms/SelectLanguage/SelectLanguage_Form.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Data;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Bank.Structure.Forms.SelectLanguage
+{
+  public partial class SelectLanguage_Form : Form
+  {
+    public SelectLanguage_Form()
+    {
+      InitializeComponent();
+    }
+    
+    //===============================================//
+
+    #region Form Load Event
+    
+    private void SelectLanguage_Form_Load(object sender, EventArgs e) => StartTimer();
+
+    private void StartTimer() => DateTimer.Enabled = true;
+
+    #endregion
+    
+    //===============================================//
+
+    #region Button Click Event
+    
+    private void SelectLanguage_Button_Click(object sender, EventArgs e)
+    {
+      bool IsLanguageSelected() => comboBox1.SelectedIndex != -1;
+      string language = Convert.ToString(comboBox1.SelectedItem);
+
+      SelectLanguage.SetInSetting(language);
+
+      if (IsLanguageSelected())
+      {
+        CloseForm();
+      }
+      else
+      {
+        ShowErrorMessage();
+      }
+    }
+
+    private void CloseForm()
+    {
+      Program.Close_SelectLanguageForm = true;
+      Close();
+    }
+
+    private void ShowErrorMessage()
+    {
+      string persianMessage = "ابتدا زبان خود را انتخاب کنید";
+      string englishMessage = "First Select your Language";
+      MessageBox.Show($"{englishMessage} \n {persianMessage}");
+    }
+
+    #endregion
+    
+    //===============================================//
+
+    #region Timer Tick Event
+
+    private void DateTimer_Elapsed(object sender, ElapsedEventArgs e)
+    {
+      SetDate();
+      SetTime();
+    }
+    
+    private void SetDate() => Date_ToolStrip.Text = DateTime.Now.ToShortDateString();
+    
+    private void SetTime() => Time_ToolStrip.Text = DateTime.Now.ToShortTimeString();
+
+    #endregion
+  }
+}
